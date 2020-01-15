5e4828a15e4fadfabbc3aa1f819d725805feb570
Denmark-banana <neat507kr@gmail.com>
2020-01-15 22:20:51 +0900

    Wetube - add Comments model and connect Video model2

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -4,6 +4,7 @@ import Video from "../models/Video";
 export const home = async(req, res) => {
     try {
         const videos = await Video.find({});
+        //console.log(videos);
         res.render('home', { pageTitle: 'Home', videos });
     } catch (error) {
         console.log(error);
@@ -21,14 +22,17 @@ export const search = (req, res) => {
 export const getUpload = (req, res) =>
     res.render('upload', { pageTitle: 'Upload' });
 
-export const postUpload = (req, res) => {
-    const { body } = req;
-    console.log(body);
-    // To Do : Upload and Save
-    // fake Id
-    res.render('upload', { pageTitle: 'Upload' });
-    //downlink : http://bbb3d.renderfarming.net/download.html
-    //res.redirect(routes.videoDetail(324393));
+export const postUpload = async(req, res) => {
+    const {
+        body: { title, description },
+        file: { path }
+    } = req;
+    const newVideo = await Video.create({
+        fileUrl: path,
+        title,
+        description
+    });
+    res.redirect(routes.videoDetail(newVideo.id));
 }
 export const videoDetail = (req, res) =>
     res.render('videoDetail', { pageTitle: 'Video Detail' });
