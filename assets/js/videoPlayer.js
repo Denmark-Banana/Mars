b10208e4fca5da142862a9da102eec3e459bcb31
Denmark-banana <neat507kr@gmail.com>
2020-02-02 18:46:04 +0900

    add Ajax API for Video View by fetch API

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -7,6 +7,13 @@ const currentTime = document.getElementById('currentTime');
 const totalTime = document.getElementById('totalTime');
 const volumeRange = document.getElementById('jsVolume');
 
+const registerView = () => {
+  const videoId = window.location.href.split("/videos/")[1];
+  fetch(`/api/${videoId}/view`, {
+    method: "POST"
+  });
+};
+
 function handlePlayClick() {
   if (videoPlayer.paused) {
     videoPlayer.play();
@@ -87,6 +94,7 @@ function setTotalTime() {
 }
 
 function handleEnded() {
+  registerView();
   videoPlayer.currentTime = 0;
   playBtn.innerHTML = '<i class="fas fa-play"></i>';
 }
@@ -106,6 +114,7 @@ function handleDrag(event) {
 }
 
 function init() {
+  videoPlayer.currentTime = 630;
   videoPlayer.volume = 0.5;
   playBtn.addEventListener('click', handlePlayClick);
   volumeBtn.addEventListener('click', handleVolumeClick);
--- routes.js
+++ routes.js
@@ -29,6 +29,12 @@ const GITHUB_CALLBACK = '/auth/github/callback';
 const FB = '/auth/facebook';
 const FB_CALLBACK = '/auth/facebook/callback';
 
+// API
+
+const API = "/api";
+const REGISTER_VIEW = "/:id/view";
+
+
 
 const routes = {
   home: HOME,
@@ -69,7 +75,9 @@ const routes = {
   gitHub: GITHUB,
   githubCallback: GITHUB_CALLBACK,
   facebook: FB,
-  facebookCallback: FB_CALLBACK
+  facebookCallback: FB_CALLBACK,
+  api: API,
+  registerView: REGISTER_VIEW
 };
 
 export default routes;
