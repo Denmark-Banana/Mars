c2810c2d336503b1177ea8a7f5750a979937f6b4
Denmark-banana <neat507kr@gmail.com>
2020-02-02 20:54:10 +0900

    userController getMe function follow userDetail function

--- controllers/userController.js
+++ controllers/userController.js
@@ -114,8 +114,15 @@ export const logout = (req, res) => {
   res.redirect(routes.home);
 };
 
-export const getMe = (req, res) => {
-  res.render('userDetail', { pageTitle: 'User Detail', user: req.user });
+export const getMe = async (req, res) => {
+  try{
+    const user = await User.findById({ _id : req.user.id}).populate("videos");
+    res.render('userDetail', { pageTitle: 'User Detail', user });
+  } catch(error){
+    console.log(error);
+    res.redirect(routes.home);
+  }
+  // res.render("userDetail", { pageTitle: "User Detail", user: req.user });
 }
 
 export const userDetail = async(req, res) => {
